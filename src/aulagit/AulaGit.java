b0ea13852529429313524553a1c61ee6c65c6243
rafae <rafae@RRBIEHL>
2024-10-16 22:29:35 -0300

    xiru 2.0

--- src/aulagit/AulaGit.java
+++ src/aulagit/AulaGit.java
@@ -17,6 +17,10 @@ public class AulaGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hello wolrd");
+        System.out.println("programa mutante");
+        if (true){
+            System.out.println("sou lindao");
+        }
     }
     
 }
